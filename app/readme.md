2cf0307f0ffda65b74c703aeee8e81fd5a7991d9
DDR <d@ddr0.ca>
2019-11-26 20:19:06 -0800

    Added eslint as a dev dependancy, so we can have consistent Javascript.

---
+++ app/readme.md
@@ -0,0 +1,10 @@
+To get started, in the project folder, run:
+
+	sudo apt install nodejs npm
+	npm install
+
+Then, to lint your files, run:
+
+	npx eslint index.js
+
+You'll probably want to use an editor with support, however.
--- util/watch-computer.sh
+++ util/watch-computer.sh
@@ -8,20 +8,25 @@ cd "$(dirname "$0")/.." #Always sync project root directory.
 
 #The following loop works fine in Konsole but won't ever stop on Gnome Terminal.
 while true; do 
-	find -regex '\./[^_\.].*' ! -name git_description ! -path 'util/stats_reported' | entr -d bash -c "
-		if [[ \$(cat git_description) != \$(git describe --tags --always) ]]; then #Only update when description changes, results in some thrashing otherwise when using --times.
-			git describe --tags --always > git_description
-		fi
-		
-		#Use --times instead of --checksum because it means we can touch a file (ie, save it) and have the camera script restart. Checksum is more proper, and may take longer to run, but I haven't noticed any difference in my testing. Note that with --times, we redeploy the whole project when we switch computers, because we ran 'git checkout' at different times.
-		rsync --recursive --delete --links --inplace --times --itemize-changes \
-			./ \"${CAM_ADDRESS:-root@192.168.12.1}:/root/web/\" \
-			--rsh=\"/usr/bin/sshpass -p $CHRONOS_PASSWORD ssh -l root\" \
-			--exclude \"__pycache__\" \
-			--exclude \"/.git\" \
-			--exclude \".mypy_cache\" \
-			--exclude \"util/stats_reported\" \
-			--exclude \"src/read_jog_wheel_encoder\" \
-			--exclude \".directory\" \
-	" || true
+	find -regex '\./[^_\.].*' \
+		! -name 'git_description' \
+		! -path 'util/stats_reported*' \
+		! -path './node_modules*' \
+		| entr -d bash -c "
+			if [[ \$(cat git_description) != \$(git describe --tags --always) ]]; then #Only update when description changes, results in some thrashing otherwise when using --times.
+				git describe --tags --always > git_description
+			fi
+			
+			#Use --times instead of --checksum because it means we can touch a file (ie, save it) and have the camera script restart. Checksum is more proper, and may take longer to run, but I haven't noticed any difference in my testing. Note that with --times, we redeploy the whole project when we switch computers, because we ran 'git checkout' at different times.
+			rsync --recursive --delete --links --inplace --times --itemize-changes \
+				./ \"${CAM_ADDRESS:-root@192.168.12.1}:/root/web/\" \
+				--rsh=\"/usr/bin/sshpass -p $CHRONOS_PASSWORD ssh -l root\" \
+				--exclude \"__pycache__\" \
+				--exclude \"/.git\" \
+				--exclude \"node_modules\" \
+				--exclude \".mypy_cache\" \
+				--exclude \"util/stats_reported\" \
+				--exclude \"src/read_jog_wheel_encoder\" \
+				--exclude \".directory\" \
+		" || true
 done
