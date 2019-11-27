52b187283b552ee3a13803e470d4105ae032c2db
DDR <d@ddr0.ca>
2019-11-26 21:40:36 -0800

    Use built-in sha256 if available, saving loading 500loc of sha library.

--- app/index.js
+++ app/index.js
@@ -1,16 +1,27 @@
 "use strict"
 
-document.querySelector('#password').addEventListener('change', evt => {
-	console.log('changing', evt.target.value, sha256(evt.target.value))
+document.querySelector('#password').addEventListener('change', async evt => {
 	
-	const passwordHash = sha256(evt.target.value)
+	//Use the built-in crypto.subtle library if available, or the sha256 library if not.
+	//eg, "test" is "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+	const sha256 = window.sha256 || (async text => 
+		Array.prototype.map.call(new Uint8Array(
+			await crypto.subtle.digest(
+				"SHA-256", 
+				new TextEncoder("utf-8").encode(text)
+			)
+		),
+		x => ('00' + x.toString(16)).slice(-2)
+	).join(''))
+	
+	console.log('changing', evt.target.value, await sha256(evt.target.value))
 	
 	fetch('/v0/authenticate', {
 		method: "POST",
 		cache: "no-cache",
 		credentials: "same-origin",
 		headers: {"Content-Type": "application/json; charset=utf-8"},
-		body: JSON.stringify(passwordHash),
+		body: JSON.stringify(await sha256(evt.target.value)),
 	})
 	.then(reply => console.info('http auth reply', reply))
 })
